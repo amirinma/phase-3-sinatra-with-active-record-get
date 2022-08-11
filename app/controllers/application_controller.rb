8733d0fee8809fbf7a094a2bed0105567e60f583
Mohammad <zia.ro@live.com>
2022-08-11 10:01:00 -0400

    commit tree 131056caab370a60f51982ca9f3a2196be25c5fe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < Sinatra::Base
-
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+  get '/games' do
+    game = Game.all.order(:title).limit(10)
+    game.to_json
   end
-
+get '/games/:id' do
+  game = Game.find(params[:id])
+  game.to_json(include: {reviews:{include: :user}})
+end
 end
